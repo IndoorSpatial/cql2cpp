a1734aed40416d2c1dfd89ad5ee8cfa26a94f3ff
Kunlin Yu <yukunlin@syriusrobotics.com>
2024-12-18 17:09:09 +0800

    Add conanfile.txt and an empty cmake list file
    Signed-off-by: Kunlin Yu <yukunlin@syriusrobotics.com>

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # cql2cpp
 c++ library for OGC CQL2(Common Query Language)
+
+## build
+```bash
+conan install . --output-folder build
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=build -DCMAKE_MODULE_PATH=build
+```
---
+++ src/main.cc
@@ -0,0 +1,13 @@
+/*
+ * File Name: main.cc
+ *
+ * Copyright (c) 2024 IndoorSpatial
+ *
+ * Author: Kunlin Yu <yukunlin@syriusrobotics.com>
+ * Create Date: 2024/12/18
+ *
+ */
+
+int main(int argc, char** argv) {
+  return 0;
+}
