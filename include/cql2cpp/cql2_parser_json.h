f72d2ca1f7ecbace36fd88c0cc1740e7006b86d3
Kunlin Yu <yukunlin@syriusrobotics.com>
2025-01-05 10:54:45 +0800

    Add recursive structure of json parser
    Signed-off-by: Kunlin Yu <yukunlin@syriusrobotics.com>

--- include/cql2cpp/cql2_parser_json.h
+++ include/cql2cpp/cql2_parser_json.h
@@ -10,6 +10,8 @@
 
 #pragma once
 
+#include <geos/vend/include_nlohmann_json.hpp>
+
 #include "ast_node.h"
 
 namespace cql2cpp {
@@ -21,9 +23,47 @@ class Cql2ParserJson {
  public:
   Cql2ParserJson() {}
 
-  bool parse() { return true; }
+  AstNodePtr parse(const geos_nlohmann::json& j) {
+    AstNodePtr node;
+    std::vector<geos_nlohmann::json> children;
+    if (parse(j, &node, &children))
+      for (const auto& child_j : children) {
+        AstNodePtr child_node = parse(child_j);
+        if (child_node != nullptr)
+          node->append(child_node);
+        else
+          return nullptr;
+      }
+
+    return node;
+  }
 
   AstNodePtr root() const { return root_; }
+
+ private:
+  bool parse(const geos_nlohmann::json& j, AstNodePtr* node,
+             std::vector<geos_nlohmann::json>* children);
+};
+
+class JsonParser {
+ public:
+  virtual bool detect(const geos_nlohmann::json& j) = 0;
+  virtual bool parse(const geos_nlohmann::json& j, AstNodePtr* node,
+                     std::vector<geos_nlohmann::json>* children) = 0;
+};
+
+class JsonParserOperator : public JsonParser {
+ public:
+  JsonParserOperator() {}
+
+  bool detect(const geos_nlohmann::json& j) override {
+    return j.contains("op");
+  }
+
+  bool parse(const geos_nlohmann::json& j, AstNodePtr* node,
+             std::vector<geos_nlohmann::json>* children) override {
+    return false;
+  }
 };
 
 }  // namespace cql2cpp
